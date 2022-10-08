96b678125ede3e573214aa8d6697df28f0952be9
gusev76 <gusev76dn@mail.ru>
2022-10-08 14:38:37 +0300

    добавили папку WORK13

---
+++ WORK13/Program.cs
@@ -0,0 +1,20 @@
+﻿// задача 13
+Console.WriteLine("Введите число: ");
+string userNumber = Console.ReadLine();
+int num = int.Parse(userNumber);
+int numberLength = userNumber.Length;
+
+if (num < 0) numberLength = userNumber.Length-1;
+
+if (numberLength >= 3 && num > 0)
+{
+    Console.WriteLine("третья цифра числа: " + userNumber[2]);
+}
+else if (numberLength >= 3 && num < 0)
+{
+    Console.WriteLine("третья цифра числа: " + userNumber[3]);
+}
+else
+{
+    Console.WriteLine("в числе нет третьей цифры");
+}
