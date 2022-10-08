2aa612e64efe314ba942127fc296b835fe16146e
gusev76 <gusev76dn@mail.ru>
2022-10-08 11:52:07 +0300

    добавили папку WORK10

---
+++ WORK10/Program.cs
@@ -0,0 +1,14 @@
+﻿// задача 10
+Console.WriteLine("Введите трехзначное число: ");
+string N1 = Console.ReadLine();
+int N = int.Parse(N1);
+
+if (N > 99 & N < 1000)
+{
+    Console.Write("вторая цифра: ");
+    Console.WriteLine(N % 10);
+}
+else
+{
+    Console.WriteLine("это не трехзначное число");
+}
