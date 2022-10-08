5e91ce984d9a2503cdf7b2d113423a651fce7f6c
gusev76 <gusev76dn@mail.ru>
2022-10-08 13:41:31 +0300

    добавили папку WORK15

---
+++ WORK15/Program.cs
@@ -0,0 +1,33 @@
+﻿// задача 15
+
+Console.WriteLine("Ввкдите желаемый день недели, от 1 до 7: ");
+string value = Console.ReadLine();
+int a = int.Parse(value);
+if (a == 1)
+{
+    Console.WriteLine ("Понедельник - рабочий день");
+}
+else if (a == 2)
+{
+    Console.WriteLine("Вторник - рабочий день");
+}    
+else if (a == 3)
+{
+    Console.WriteLine("Среда -рабочий день");
+}
+else if (a == 4)
+{
+    Console.WriteLine("Черверг - рабочий день");
+}    
+else if (a == 5)
+{
+    Console.WriteLine("Пятница - рабочий день");
+}
+else if (a == 6)
+{
+    Console.WriteLine("Суббота - выходной день");
+}
+else if (a == 7)
+{
+    Console.WriteLine("Воскресенье - выходной день");
+}
